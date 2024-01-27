71842f8cca5338557ec8579a2307ffc2f5f757ce
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-01-27 11:18:52 +0700

    làm trang danh sách sản phầm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,8 +1,19 @@
-const products = require("../../models/product.model")
+const Product = require("../../models/product.model");
 
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: 'active',
+    deleted: false
+  });
+
+  const newProduct = products.map((item) => {
+    item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed();
+    return item;
+  })
+  console.log(newProduct);
+
   res.render("client/pages/products/index.pug", {
     pageTitle: "Sản Phẩm",
-    products: products
+    products: newProduct,
   });
 }
