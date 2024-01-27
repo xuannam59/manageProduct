0196a187651517b1630668821dcf012e5d1ac1bd
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-01-27 10:59:15 +0700

    cài đặt mongoose

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,8 @@
+const products = require("../../models/product.model")
+
 module.exports.index = (req, res) => {
   res.render("client/pages/products/index.pug", {
-    pageTitle: "Sản Phẩm"
+    pageTitle: "Sản Phẩm",
+    products: products
   });
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
+const database = require("./config/database")
 
 const route = require('./router/client/index.route')
+
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -11,7 +13,8 @@ app.use(express.static('public'))
 
 // route
 route(app);
-
+//
+database.connect();
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
