c05c643e4c68c397e444be406d80d335ba102ddd
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-01-27 09:07:54 +0700

    cấu hình controller

--- router/client/home.route.js
+++ router/client/home.route.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render("client/pages/home/index", {
-    titlePage: "Trang chủ"
-  });
-})
+const controller = require("../../controllers/client/home.controller");
+
+router.get('/', controller.index)
 
 module.exports = router;
--- router/client/product.route.js
+++ router/client/product.route.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const controller = require("../../controllers/client/product.controller")
 
-router.get('/', (req, res) => {
-  res.render("client/pages/products/index.pug", {
-    titlePage: "Sản Phẩm"
-  });
-})
+router.get('/', controller.index)
 
 module.exports = router;
