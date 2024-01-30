345f79c6541f02ec106ea12da1faaac3a1f4a4ad
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-01-30 15:59:44 +0700

    3.Tính năng bộ lọc trạng thái

--- controllers/admin/product.control.js
+++ controllers/admin/product.control.js
@@ -2,12 +2,42 @@ const Product = require("../../models/product.model")
 
 // [GET] /adimin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ];
+
+  if (req.query.status) {
+    const index = filterStatus.findIndex(item => item.status == req.query.status)
+    filterStatus[index].class = "active"
+  } else {
+    filterStatus[0].class = "active"
+  }
+
+  let find = {
     deleted: false
-  });
-  console.log(products);
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  const products = await Product.find(find);
   res.render("admin/pages/products/index", {
     pageTitle: "trang Sản phẩm",
-    products: products
+    products: products,
+    filterStatus: filterStatus
   });
 }
---
+++ public/admin/js/script.js
@@ -0,0 +1,19 @@
+// Button Status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if (status) {
+        url.searchParams.set("status", status);
+      }
+      else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url.href;
+    })
+  })
+}
+// End Button Status
