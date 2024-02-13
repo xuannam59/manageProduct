1c39a05da1f96fc8bee639f67d774acf6a234bce
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-02-13 16:28:18 +0700

    8.1 Xoá mềm

--- controllers/admin/product.control.js
+++ controllers/admin/product.control.js
@@ -78,7 +78,12 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.deleteOne({ _id: id });
+  // await Product.deleteOne({ _id: id });
+  await Product.updateOne({ _id: id },
+    {
+      deleted: true,
+      deletedAt: new Date()
+    });
 
   res.redirect('back');
 }
