5413f05785fa68dcacbbbfa3ee4e2ab8d36699ad
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-04-13 17:35:59 +0700

    make authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,13 +1,21 @@
-const Accounts = require("../../models/account.model");
-const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/roles.model");
+const systemConfig = require("../../config/system");
 
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
-  const user = await Account.findOne({ token: token });
+  const user = await Account.findOne({ token: token }).select("-password");
   if (!user) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     return;
   }
+
+  const role = await Role.findOne({
+    _id: user.role_id
+  }).select("title permissions");
+
+  res.locals.user = user;
+  res.locals.role = role;
+
   next();
 }
